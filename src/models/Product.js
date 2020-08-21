5a1f6155065daa667db02934f1e6ab90693a5114
gonzalo <gonzalobrolyn@gmail.com>
2020-08-20 20:09:46 -0500

    V1.3 CRUD de productos

---
+++ src/models/Product.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+const {Schema} = mongoose
+
+const ProductSchema = new Schema ({
+  nombre: {type: String, required: true},
+  descripcion: {type: String, required: true},
+  fecha: {type: Date, default: Date.now}
+})
+
+module.exports = mongoose.model('Product', ProductSchema)
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,8 +1,55 @@
 const express = require('express')
 const router = express.Router()
+const Product = require('../models/Product')
 
-router.get('/products', (req, res) => {
-  res.send('Products')
+router.get('/products/add', (req, res) => {
+  res.render('products/new-product')
+})
+
+router.post('/products/new-product', async (req, res) => {
+  const {nombre, descripcion} = req.body
+  const errors = []
+  if(!nombre) {
+    errors.push({text: 'Escribe el nombre del producto'})
+  }
+  if(!descripcion) {
+    errors.push({text: 'Escribe la descripción del producto'})
+  }
+  if(errors.length > 0){
+    res.render('products/new-product', {
+      errors,
+      nombre,
+      descripcion
+    })
+  } else {
+    const newProduct = new Product({nombre, descripcion})
+    await newProduct.save()
+    req.flash('success_msg', 'Producto agregado correctamente')
+    res.redirect('/products')
+  }
+})
+
+router.get('/products', async (req, res) => {
+  const products = await Product.find().lean().sort({fecha: 'desc'})
+  res.render('products/all-products', {products})
+})
+
+router.get('/products/edit/:id', async (req, res) => {
+  const product = await Product.findById(req.params.id).lean()
+  res.render('products/edit-product', {product})
+})
+
+router.put('/products/edit-product/:id', async (req, res) => {
+  const {nombre, descripcion} = req.body
+  await Product.findByIdAndUpdate(req.params.id, {nombre, descripcion})
+  req.flash('success_msg', 'Producto actualizado correctamente')
+  res.redirect('/products')
+})
+
+router.delete('/products/delete/:id', async (req, res) => {
+  await Product.findByIdAndDelete(req.params.id)
+  req.flash('success_msg', 'Producto eliminado correctamente')
+  res.redirect('/products')
 })
 
 module.exports = router
